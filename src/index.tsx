0aadc70941bccf90139cf7f757c2f8a74a87589f
devepdogukan <dogukan@finfree.co>
2024-10-28 23:55:51 +0300

    update index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import App from '~/order-history'
+import { Provider } from 'react-redux'
 
+const loadStore = async () => {
+  const storeModule = await import('container/store')
+  return storeModule.default
+}
+
+const store = loadStore()
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<App />)
+store.then((actualStore) =>
+  root.render(
+    <Provider store={actualStore}>
+      <App />
+    </Provider>,
+  ),
+)
